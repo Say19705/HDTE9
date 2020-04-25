054d476fe93feb2caf3aa7978d23ad38525e90ce
Say19705 <54776073+Say19705@users.noreply.github.com>
2020-04-25 13:41:30 -0600

    Update Factory.java

--- src/Factory.java
+++ src/Factory.java
@@ -1,4 +1,17 @@
-
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.TreeMap;
 public class Factory {
+	public Map getType(int op){
+		if(op == 1) {
+			return new hashMap();
+		}
+		if(op == 2) {
+			return new SplayTree();
+		}
+		else
+			return new hashMap();
+	}
 
 }
